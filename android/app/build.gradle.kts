185e6579269530b9d1b14ac5355e41fe012f44c3
Matty77o <mattywood30@gmail.com>
2026-09-09 19:17:58 +0100

    Add files via upload

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.matty77o.umaytvguide"
         minSdk = 26
         targetSdk = 35
-        versionCode = 63
-        versionName = "4.0.1"
+        versionCode = 64
+        versionName = "4.0.2"
     }
 
     compileOptions {
