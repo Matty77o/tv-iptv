b5a32c0ef2b70ea2ce0fcf0721b333e7ef73f45f
Matty77o <mattywood30@gmail.com>
2026-09-09 12:17:34 +0100

    Update build_epg.py

--- build_epg.py
+++ build_epg.py
@@ -21,7 +21,6 @@ CHANNELS = {
     "MOONBUG.KIDS.TV.tr": "Moonbug Kids",
     "plex.tv.BABY.SHARK.TV.plex": "Baby Shark TV",
     "PBS.Kids.Stream.us2": "PBS KIDS",
-    "plex.tv.Baby.Einstein.plex": "Baby Einstein",
 
     # Turkish / Kids
     "TRT.ÇOCUK.HD.tr": "TRT Çocuk",
